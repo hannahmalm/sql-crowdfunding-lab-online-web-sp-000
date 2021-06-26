cede32d0e5e752ef5aa9beb3cb657dd23eaae7df
Hannah M <hannahemalm616@gmail.com>
2021-06-26 18:04:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,21 +12,14 @@ require 'pry' # Write your sql queries in this file in the appropriate method li
 #ON table1.column_name = table2.column_name;
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-# "SELECT title, pledges.amount FROM projects INNER JOIN projects ON pledges.id = projects.id ORDER BY projects.title ASC;"
 "SELECT projects.title, SUM(pledges.amount) from projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  #name and age is within the users table 
-  #amount is within the pledges table 
-  #ORDER BY name 
   "SELECT users.name,users.age, SUM(pledges.amount) FROM users INNER JOIN pledges ON users.id = pledges.user_id GROUP BY name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  #title is within the project table 
-  #amount is within the pledges table 
-  #sum of the funding_goal in projects 
   "SELECT projects.title,(SUM(pledges.amount)-funding_goal) AS goal_fund FROM pledges INNER JOIN projects ON pledges.project_id = projects.id GROUP BY title HAVING goal_fund >= 0"
 end
 
